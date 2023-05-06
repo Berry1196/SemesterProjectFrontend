1097aac3cd7322dcb0e58f354b61ee35ecb1bbc3
Berry1196 <Sebastianberry11@gmail.com>
2023-05-06 19:56:34 +0200

    Test cloud api

--- src/ApiFacade.js
+++ src/ApiFacade.js
@@ -19,7 +19,8 @@ function apiFacade() {
       username: username,
       password: password,
     });
-    const data = await fetch("http://localhost:8080/api/login", options);
+    const data = await fetch("http://berrywebsite.dk/api/login", options);
+    //const data = await fetch("http://localhost:8080/api/login", options);
     const res = await data.json();
     return res;
   }
@@ -48,6 +49,7 @@ function apiFacade() {
     const blob = await data.blob();
     const imageFile = new Blob([blob]);
     const imageUrl = URL.createObjectURL(imageFile);
+    console.log(muscles)
     return imageUrl;
   }
 
