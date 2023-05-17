8fcfeed702cbb176de73bb00b96bf6a0eaab193d
Berry1196 <Sebastianberry11@gmail.com>
2023-05-17 09:50:04 +0200

    fixed divs around buttons

--- src/routes/guest/Login.jsx
+++ src/routes/guest/Login.jsx
@@ -76,14 +76,12 @@ const Login = () => {
                 </div>
               </div>
 
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
             </form>
           </div>
         </div>
